0c21b0e42e5b76e1be95346b62b6b6cb74c10237
masima305 <masima305@gmail.com>
2020-08-03 21:40:34 +0900

    MySQL 연결

---
+++ target/test-classes/com/myfinance/sprapp/com/myfinance/sprapp/testDataSource.java
@@ -0,0 +1,24 @@
+package com.myfinance.sprapp.com.myfinance.sprapp;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+import java.sql.Connection;
+
+import javax.inject.Inject;
+import javax.sql.DataSource;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+
+public class testDataSource {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
